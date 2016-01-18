f6fc19dda4883e2bf35ae77da5ae5d9d736002b1
Brian Thompson <habajca@gmail.com>
2016-01-18 12:33:06 -0800

    Refactor timestamp generation and add tests to encompass random logic.

--- generation/timestamp.go
+++ generation/timestamp.go
@@ -3,15 +3,9 @@ package generation
 import "math/rand"
 
 func randomTimestamp(origin int64, timeDistance int) int64 {
-	return generateTimestamp(origin, rand.Intn(timeDistance), rand.Intn(2))
+	return generateTimestamp(origin, timeDistance, rand.Int63n(2*int64(timeDistance)+1))
 }
 
-func generateTimestamp(origin int64, timeDistance int, positiveBoolAsInt int) int64 {
-	if positiveBoolAsInt == 1 {
-		return origin + int64(timeDistance)
-	}
-	if positiveBoolAsInt == 0 {
-		return origin + -1*int64(timeDistance)
-	}
-	panic("positiveBoolAsInt must be 0 or 1.")
+func generateTimestamp(origin int64, timeDistance int, randOffset int64) int64 {
+	return origin - int64(timeDistance) + randOffset
 }
--- generation/timestamp_test.go
+++ generation/timestamp_test.go
@@ -1,13 +1,68 @@
 package generation
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
+	"time"
 )
 
-// Note: generation.randomTimestamp is not tested as it is random by nature and testing could result in inconsistent results. generation.generateTimestamp, which contains all non-random logic is tested.
+const TEST_RANDOM_TIMESTAMP_RUNS = 1000
+const TEST_TIME_DISTANCE = 1
+const TEST_TIME_ORIGIN = 0
+const TEST_TIME_RANGE_START = TEST_TIME_ORIGIN - TEST_TIME_DISTANCE
+const TEST_TIME_RANGE_END = TEST_TIME_ORIGIN + TEST_TIME_DISTANCE
+
+// Note: this test involves randomness and therefore could in extremely rare cases have inconsistent results.
+func TestRandomTimestamp(t *testing.T) {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
+	covered := make(map[int64]bool)
+	for i := 0; i < TEST_RANDOM_TIMESTAMP_RUNS; i++ {
+		randTimestamp := randomTimestamp(TEST_TIME_ORIGIN, TEST_TIME_DISTANCE)
+		if !assert.True(
+			t,
+			randTimestamp >= TEST_TIME_RANGE_START,
+			fmt.Sprintf(
+				"%d is not between %d and %d (inclusive). Seed: %d.",
+				randTimestamp,
+				TEST_TIME_RANGE_START,
+				TEST_TIME_RANGE_END,
+				seed,
+			),
+		) {
+			t.FailNow()
+		}
+		if !assert.True(
+			t,
+			randTimestamp <= TEST_TIME_RANGE_END,
+			fmt.Sprintf(
+				"%d is not between %d and %d (inclusive). Seed: %d.",
+				randTimestamp,
+				TEST_TIME_RANGE_START,
+				TEST_TIME_RANGE_END,
+				seed,
+			),
+		) {
+			t.FailNow()
+		}
+		covered[randTimestamp] = true
+	}
+	for i := -1; i <= 1; i++ {
+		assert.True(
+			t,
+			covered[int64(i)],
+			fmt.Sprintf(
+				"%d was not covered in %d runs of randomTimestamp. Seed: %d.",
+				i,
+				TEST_RANDOM_TIMESTAMP_RUNS,
+				seed,
+			),
+		)
+	}
+}
 
 func TestGenerateTimestamp(t *testing.T) {
-	assert.Equal(t, int64(-10), generateTimestamp(0, 10, 0))
-	assert.Equal(t, int64(10), generateTimestamp(0, 10, 1))
+	assert.Equal(t, int64(5), generateTimestamp(0, 5, 10))
 }
